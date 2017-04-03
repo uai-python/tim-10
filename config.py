96154cdc57948e7694c96e9e078b380a1f566335
nasiahur <Nasiahur@gmail.com>
2017-04-03 05:04:52 -0700

    Add files via upload

---
+++ config.py
@@ -0,0 +1,9 @@
+WTF_CSRF_ENABLED=True
+SECRET_KEY='help-me-will-ya-!'
+
+import os
+basedir=os.path.abspath(os.path.dirname(__file__))
+
+SQLALCHEMY_DATABASE_URI='sqlite:///' +os.path.join(basedir, 'app.db')
+SQLALCHEMY_MIGRATE_REPO=os.path.join(basedir, 'db_repository')
+
